95d9217e5813b13f38bd99c3a56b9943b0a39952
Franz Koch <franz.koch@elements-net.de>
2013-01-28 23:01:31 +0100

    [Task] add a development context and load the configuration from this context instead.

--- inc/config.php
+++ inc/config.php
@@ -5,8 +5,26 @@
  * Thus include 'em from an external file but
  * keep the dummy config.php for myBB
  */
-$private_path = '/etc/xbmc/php-include';
-set_include_path(get_include_path().PATH_SEPARATOR.$private_path);
-require_once('forum/private/configuration.php');
- 
+
+/**
+ * This sets the context the application is running in.
+ * It allows to change certain behavior depending on context.
+ * The currently supported contexts are:
+ *
+ * development		Used in the development environment
+ * production		Used on the live website
+ */
+$context = @is_file(dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'developmentConfiguration.php') ? 'development' : 'production';
+define('CONTEXT', $context);
+
+
+// include the context depending configuration at the bottom
+// which allows to override any default configuration if needed
+if (CONTEXT == 'development') {
+	require_once('developmentConfiguration.php');
+} else {
+	$private_path = '/etc/xbmc/php-include';
+	set_include_path(get_include_path() . PATH_SEPARATOR . $private_path);
+	require_once('forum/private/configuration.php');
+}
 ?>
