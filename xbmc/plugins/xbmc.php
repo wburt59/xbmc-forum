2b7bfc99b5ed9ee812235d6586a584435c7b40be
da-anda <da-anda@xbmc.org>
2014-11-02 20:47:07 +0100

    [Task] updated google analytics after domain switch

--- xbmc/plugins/xbmc.php
+++ xbmc/plugins/xbmc.php
@@ -448,7 +448,7 @@ function xbmc_AddToFooter($page) {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-3066672-3', 'xbmc.org');
+  ga('create', 'UA-3066672-3', 'auto');
   ga('send', 'pageview');
 
 </script>
