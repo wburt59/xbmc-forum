c99219f883a9edc4e0f19931c3f12016b53aec82
kib <edeklerk@gmail.com>
2015-04-11 18:56:49 +0200

    Clarify registration and account recovery strings Start using accounthelp email instead of having people send to contact

--- xbmc/languages/english/xbmc.lang.php
+++ xbmc/languages/english/xbmc.lang.php
@@ -49,4 +49,8 @@ $l['toplinks_memberlist'] = 'Members';
 $l['editor_title_imgur'] = 'Insert imgur.com gallery';
 $l['editor_enter_imgur'] = 'Enter the id from your imgur gallery, like "http://imgur.com/gallery/aBcDeF" -> "aBcDeF"';
 
+// registration and account recovery
+$l['redirect_registered_activation'] = "Thank you for registering on {1}, {2}.<p>To complete your registration, please check your email for account activation instructions. Until you activate your account you may not be able to post on these forums. Be sure to check your spam folders as well as your regular inbox for the activation email.";
+$l['redirect_activationresent'] = "Your activation e-mail has been resent. Be sure to check your spam folders as well as your regular inbox. If you do not receive the activation email within a day, contact the site admins at accounthelp@kodi.tv. Make sure you include the username you registered and your emailadress and we will try to resolve it.";
+$l['error_spam_deny'] = "Our systems detect that you may be a spammer and therefore you have been denied registration. The most common reason for this is when you are connecting through a proxy that has been misused by spammers. Please contact the site admins at accounthelp@kodi.tv and be sure to include the following information: username and ipadress (see <a href=\"http://whatsmyipaddress.org\" target=\"_blank\">whatsmyipadress.org</a>) and we will try to resolve it.";
 ?>
